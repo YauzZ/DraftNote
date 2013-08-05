2bf4222abd373871eb178175f28f5f2d14448117
Colin Yang Hong <colinyang@nmg.com.hk>
2013-08-05 14:49:22 +0800

    Initial Commit

---
+++ DraftNote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DraftNote
+//
+//  Created by Colin Yang Hong on 13-8-5.
+//  Copyright (c) 2013年 Colin Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
