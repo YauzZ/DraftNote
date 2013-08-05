c63e55ee1241bfce48bb4c4ec04905181cc4a605
Colin Yang Hong <colinyang@nmg.com.hk>
2013-08-05 15:03:17 +0800

    添加UI和CanvasViewController

---
+++ DraftNote/CanvasViewController.h
@@ -0,0 +1,14 @@
+//
+//  CanvasViewController.h
+//  DraftNote
+//
+//  Created by Colin Yang Hong on 13-8-5.
+//  Copyright (c) 2013年 Colin Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CanvasViewController : UIViewController
+
+@property (retain, nonatomic) IBOutlet UIToolbar *toolbar;
+@end
---
+++ DraftNote/CanvasViewController.m
@@ -0,0 +1,42 @@
+//
+//  CanvasViewController.m
+//  DraftNote
+//
+//  Created by Colin Yang Hong on 13-8-5.
+//  Copyright (c) 2013年 Colin Yang Hong. All rights reserved.
+//
+
+#import "CanvasViewController.h"
+
+@interface CanvasViewController ()
+
+@end
+
+@implementation CanvasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    [_toolbar release];
+    [super dealloc];
+}
+@end
